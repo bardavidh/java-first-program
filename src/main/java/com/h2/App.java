f78a388ce162f94eb8795dd5e2ec2c0d9511e190
H B <bardavidh@yahoo.com>
2022-09-13 16:41:44 -0400

    Update App.java

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -9,9 +9,18 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int[] a={1,2,3,4,5};
+        System.out.println(add(a));
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return (2*number);
+    }
+    private  static int add(int[] numbers){
+        int sum=0;
+        for (int i=0;i<numbers.length;i++) {
+            sum += numbers[i];
+        }
+        return sum;
     }
 }
