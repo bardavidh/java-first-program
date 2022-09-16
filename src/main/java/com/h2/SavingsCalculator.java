cf7096419b6d6e7d99fdcb7e827f42794a6e6e83
H B <bardavidh@yahoo.com>
2022-09-16 12:01:55 -0400

    Update SavingsCalculator.java

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -8,4 +8,12 @@ public SavingsCalculator(float credits[],float debits[]){
  this.credits=credits;
  this.debits=debits;
 }
+private float sumOfCredits(){
+    //return 0.0f;
+    float sum=0.0f;
+    for (int i; i<credits.length;i++){
+        sum+=credits[i];
+    }
+    return sum;
+}
 }
